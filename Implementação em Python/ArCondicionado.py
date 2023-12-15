5cf14cb844d921e8747a6d919143687d663e5fce
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2023-12-15 10:28:46 -0300

    Correção do paralelismo da função run_time, e adiçã de pequenos comentarios

--- Implementação em Python/ArCondicionado.py
+++ Implementação em Python/ArCondicionado.py
@@ -10,37 +10,49 @@ temperatura_escolhida = 25
 termostato = temperatura_ambiente
 tempo_alternar = 0
 
+
 # Função para ligar o compressor e resfriar
 def ligar_compressor():
     global termostato, compressor_ligado
+    print("Temperatura Atual:", end="")
     while temperatura_escolhida < termostato:
-        print("Temperatura Atual:", termostato)
+        print(end=f"; {termostato}")
         termostato -= 1
         time.sleep(0.15)
+
     compressor_ligado = False
-    print("Fim do resfriamento.")
+    print("\nFim do resfriamento.")
+
 
 # Função para ligar/desligar o ar condicionado
 def ligar_desligar():
-    global ar_condicionado_ligado, compressor_ligado
-    ar_condicionado_ligado = not ar_condicionado_ligado
-    if ar_condicionado_ligado:
-        compressor_ligado = True
-        ligar_compressor()
-        print("<Ar condicionado ligado.>")
+    global tempo_alternar
+    if tempo_alternar > 0:
+        tempo_alternar = 0
+        start_time(tempo_alternar)
+
     else:
-        compressor_ligado = False
-        print("<Ar condicionado desligado.>")
+        global ar_condicionado_ligado, compressor_ligado
+        ar_condicionado_ligado = not ar_condicionado_ligado
+        if ar_condicionado_ligado:
+            compressor_ligado = True
+            ligar_compressor()
+            print("<Ar condicionado ligado.>")
+        else:
+            compressor_ligado = False
+            print("<Ar condicionado desligado.>")
+
 
 # Função para executar o temporizador
-def run_timer(tempo_definido):
+def run_timer():
     global tempo_alternar
-    tempo_alternar = tempo_definido  # Define o tempo restante para ligar ou desligar o ar condicionado
-    while tempo_definido > 0:
-        tempo_definido -= 1
-        time.sleep(0.5)
-        print(f"{tempo_definido}; ", end="")
+    while tempo_alternar > 0:
+        tempo_alternar -= 1
+        time.sleep(10)
+        print(f"{tempo_alternar}; ", end="")
     print(" ")
+    ligar_desligar()
+
 
 # Função para mostrar o menu
 def mostrar_menu():
@@ -56,6 +68,13 @@ def mostrar_menu():
         print("3. Alterar modo")
         print("4. Definir Timer")
 
+
+def start_time(inicio_timer):
+    # Inicializa o temporizador em uma thread separada
+    inicio_timer = threading.Thread(target=run_timer)
+    inicio_timer.start()
+
+
 # Loop principal
 while True:
     mostrar_menu()
@@ -66,31 +85,18 @@ while True:
         ligar_desligar()
 
     elif entrada == 2 and ar_condicionado_ligado:
-        nova_temperatura = int(input("Digite a nova temperatura desejada: "))
-        temperatura_escolhida = nova_temperatura
+        temperatura_escolhida = int(input("Digite a nova temperatura desejada: "))
         print("Temperatura ajustada para", temperatura_escolhida, "°C")
 
     elif entrada == 3 and ar_condicionado_ligado:
         print("Modo de operação alterado")
-        # Adicione o código para alterar o modo de operação, se necessário
+        # Adicione o código para alterar o modo de operação
 
     elif entrada == 4:
-        tempo_definido = int(input(
+        tempo_alternar = int(input(
             f"Digite em quanto tempo o ar condicionado deve {'ligar' if not ar_condicionado_ligado else 'desligar'}: "))
-        print(f"O timer está configurado para {tempo_definido}")
-
-        # Inicializa o temporizador em uma thread separada
-        inicio_timer = threading.Thread(target=run_timer, args=(tempo_definido,))
-        inicio_timer.start()
+        print(f"O timer está configurado para {tempo_alternar}")
+        start_time(tempo_alternar)
 
     else:
         print("Digite uma opção válida")
-
-    # Verifica se o tempo para ligar ou desligar o ar condicionado foi atingido
-    if tempo_alternar > 0:
-        if tempo_alternar == 1:
-            print("Tempo esgotado!")
-            ligar_desligar()  # Liga ou desliga o ar condicionado após o tempo definido
-            tempo_alternar = 0
-        else:
-            print(f"Tempo restante: {tempo_alternar} segundos")
