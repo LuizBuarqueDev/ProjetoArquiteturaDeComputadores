3b05099e03c55bd16c8fd63177914851524037f0
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2023-12-14 23:28:50 -0300

    Implementaçõ da função run_time usando paralelismo

--- Implementação em Python/ArCondicionado.py
+++ Implementação em Python/ArCondicionado.py
@@ -1,21 +1,22 @@
 import random
 import time
+import threading
 
 ar_condicionado_ligado = False
-temperatura_escolhida = 25
-temperatura_atual = random.randint(15, 50)
 compressor_ligado = False
-
-
+temperatura_ambiente = random.randint(10, 50)
+temperatura_escolhida = 25
+termostato = temperatura_ambiente
 tempo_desligar = 0
 
 
 def ligar_compressor():
-    global temperatura_atual
-    while temperatura_escolhida < temperatura_atual:
-        print("Temperatura Atual:", temperatura_atual)
-        temperatura_atual -= 1
+    global termostato, compressor_ligado
+    while temperatura_escolhida < termostato:
+        print("Temperatura Atual:", termostato)
+        termostato -= 1
         time.sleep(0.15)
+    compressor_ligado = False
     print("Fim do resfriamento.")
 
 
@@ -25,24 +26,29 @@ def ligar_desligar():
     if ar_condicionado_ligado:
         compressor_ligado = True
         ligar_compressor()
-
+        print("<Ar condicionado ligado.>")
     else:
         compressor_ligado = False
-        print("Ar condicionado desligado.")
+        print("<Ar condicionado desligado.>")
 
-def timer ():
-    global tempo_desligar
-    while tempo_desligar > 0:
-        print(f"Tempo restante : {tempo_desligar}")
-        tempo_desligar -= 1
-        time.sleep(60)
+
+def run_timer(tempo_definido):
+    while tempo_definido > 0:
+        tempo_definido -= 1
+        time.sleep(0.5)
+        print(f"{tempo_definido}; ", end="")
+    ligar_desligar()
 
 
 while True:
     print("\nControle Ar Condicionado")
     print("Ar")
     print("1. Ligar/Desligar")
+    print(f"4. Definir Timer\n {'-' * 50}")
+
     if ar_condicionado_ligado:
+        print("**Ar Ligado**")
+        print("1. Ligar/Desligar")
         print("2. Ajustar temperatura")
         print("3. Alterar modo")
         print("4. Definir Timer")
@@ -62,10 +68,14 @@ while True:
         print("Modo de operação alterado")
         # alterar o modo de operação
 
-    elif entrada == 4 and ar_condicionado_ligado:
-        tempo_desligar = int(input("Digite o tempo para desligar (minutos): "))
-        timer()
+    elif entrada == 4:
+        tempo_definido = int(input(
+            f"Digite em quanto tempo o ar condicinado deve {"ligar" if not ar_condicionado_ligado else "desligar"}: "))
+        print(f"O timer esta configurado para {tempo_definido}")
 
+        inicio_timer = threading.Thread(target=run_timer, args=(tempo_definido,))
+        inicio_timer.start()
+        inicio_timer.join()
 
-        print(f"Ar condicionado desligará em {tempo_desligar} minutos.")
-        print("Timers configurados.")
+    else:
+        print("Digite uma opção valida")
